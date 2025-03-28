074f9b806d416e0372e2be0604147a965951c615
Yamingue <yamking01@gmail.com>
2025-03-28 10:48:05 +0100

    mise en place suivit

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use Exception;
+use Dom\Entity;
 use App\Entity\Alert;
+use App\Repository\AlertRepository;
 use App\Repository\QuartierRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Dom\Entity;
-use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 final class IndexController extends AbstractController
 {
@@ -23,14 +28,51 @@ final class IndexController extends AbstractController
         ]);
     }
 
+    #[Route('/suivi', name: 'suivi')]
+    public function suivi(Request $request, AlertRepository $alertRepository): Response
+    {
+        $alert = null;
+        $form = $this->createFormBuilder()
+            ->add('id', NumberType::class, [
+                'label' => "Numéro de l'alert",
+                'attr' => [
+                    'placeholder' => 'Entrez le code de suivi',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Rechercher',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $alert = $alertRepository->find($data['id']);
+            if (!$alert)  {
+                $this->addFlash('error', 'Aucune alerte trouvée avec ce code');
+            }
+        }
+        return $this->render('index/suivit.html.twig', [
+            'form' => $form->createView(),
+            'alert' => $alert,
+        ]);
+    }
+
+
     #[Route('/signaler', name: 'signaler')]
     function Signaler(
         Request $request,
         SerializerInterface $serializer,
         QuartierRepository $quartierRepository
-        )  {
+    ) {
 
-        $quartiers = $serializer->serialize($quartierRepository->findAll(), 'json',context:[
+        $quartiers = $serializer->serialize($quartierRepository->findAll(), 'json', context: [
             'circular_reference_handler' => function ($object) {
                 return $object->getId();
             }
@@ -41,12 +83,12 @@ final class IndexController extends AbstractController
         ]);
     }
 
-    #[Route('/api/signal', name:'saveSignale', methods:['POST'])]
+    #[Route('/api/signal', name: 'saveSignale', methods: ['POST'])]
     function saveSignale(
         Request $request,
         QuartierRepository $quartierRepository,
         EntityManagerInterface $em
-        )  {
+    ) {
         // $data = $data = $request->request->all();
         $description = $request->request->get('description');
         $quartier = $quartierRepository->find($request->request->get('quartier'));
@@ -63,9 +105,9 @@ final class IndexController extends AbstractController
         $alert->setLng($lng);
         $alert->setStatus('En attente');
         if ($photo = $request->files->get('photo')) {
-            $fileName = uniqid().'.'.$photo->guessExtension();
+            $fileName = uniqid() . '.' . $photo->guessExtension();
             $photo->move('Image/alert', $fileName);
-            $alert->setPhoto('/Image/alert/'.$fileName);
+            $alert->setPhoto('/Image/alert/' . $fileName);
         }
 
         try {
@@ -79,7 +121,7 @@ final class IndexController extends AbstractController
             ]);
         } catch (Exception $ex) {
             return $this->json([
-                'status' =>'error',
+                'status' => 'error',
                 'message' => $ex->getMessage(),
                 'code' => 500,
             ]);
