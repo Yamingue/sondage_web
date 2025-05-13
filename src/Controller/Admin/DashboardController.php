146c04b6df8e3b7e15d95a69f3008dd988e0716f
Yamingue <franck.yamingue@gmail.com>
2025-05-13 17:17:08 +0100

    change display name on admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -44,7 +44,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Sondage Web');
+            ->setTitle('MOPANDJ');
     }
 
     public function configureMenuItems(): iterable
